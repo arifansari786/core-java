f1fbd2c8e5da910c6673c1af28e7b6eeb54d781a
Arif Ansari <44662033+arifansari786@users.noreply.github.com>
2018-11-01 11:33:02 +0530

    java program to find maximum element in array

---
+++ max.java
@@ -0,0 +1,24 @@
+class abc
+{
+int max(int x[])
+{
+int m1,l;
+l=x.length;
+m1=x[0];
+for(int i=1;i<l;i++)
+{
+if(x[i] >m1)
+m1=x[i];
+}
+return m1;
+}
+public static void main(String s[])
+{
+int m;
+abc a1=new abc();
+int a[]={2,5,7,6,5,3,1,4};
+m=a1.max(a);
+System.out.println("The largest element is"+m);
+
+}
+}
